be3c2a76515746d91c5bb0256871ab98112625c6
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-11-24 16:38:57 -0800

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates file in /tmp with specific permissions, owner, and content 
 
 file { '/tmp/school':
-  ensure   => 'file',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
