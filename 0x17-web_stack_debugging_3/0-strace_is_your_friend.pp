b6907cc56d58b1a0c7789a733525598c377f7afa
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-17 19:00:04 -0800

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Script to make a a manifest to fix termintion of phpp.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
