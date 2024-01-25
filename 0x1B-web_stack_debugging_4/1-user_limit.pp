d37323895b2f2254a227d48f2958f8521cde8637
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-25 12:56:55 +0000

    updates

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable a user to login and open files without getting errors.
+
+# Increase file limit for the user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase file limit for the user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
